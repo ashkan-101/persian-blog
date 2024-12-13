d29c2b7304c7667b237a5da83da550de55ae8f75
ashkan-101 <ashkan.dev.101@gmail.com>
2024-12-14 00:54:53 +0330

    complete section repository for RegisterCode

---
+++ src/modules/registercode/repositories/contracts/IRegisterCode.PG.Repository.ts
@@ -0,0 +1,5 @@
+import IRepository from "../../../contracts/IRepository";
+import IRegisterCode from "../../entity/contracts/IRegisterCode";
+
+
+export default interface IRegisterCodePGRepository extends IRepository<IRegisterCode>{}
