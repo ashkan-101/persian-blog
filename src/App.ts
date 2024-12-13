f6bd69c935d32a09d77d24d19029cc0ea11c40c1
ashkan-101 <ashkan.dev.101@gmail.com>
2024-12-14 02:39:22 +0330

    config Boot in boot and App

--- src/App.ts
+++ src/App.ts
@@ -2,20 +2,24 @@ import express from 'express'
 import { Application } from 'express'
 import RouterService from './router/RouterService'
 import postgresConnection from './Infrastructures/connections/postgreSQL'
+import Boot from './boot/Boot'
 
 
 export default class App {
   private readonly app: Application
   private readonly port: number
   private readonly router: RouterService
+  private readonly boot: Boot
 
   constructor(port: number){
     this.app = express()
     this.port = port
     this.router = new RouterService(this.app)
+    this.boot = new Boot(this.app)
   }
 
   public start(){
+    this.boot.init()
     this.router.run()
     this.app.listen(this.port, () => {
       console.log('Application is running ...');
---
+++ src/boot/Boot.ts
@@ -0,0 +1,17 @@
+import bodyParser from "body-parser";
+import { Application } from "express";
+import cors from 'cors'
+
+
+export default class Boot {
+  private readonly app: Application
+
+  constructor(app: Application){
+    this.app = app
+  }
+
+  public init(){
+    this.app.use(cors())
+    this.app.use(bodyParser.json())
+  }
+}
